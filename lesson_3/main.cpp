d896d763d824eab8b84313c94f3392b7fe9ec124
mehebbetfz <me.farzaliyev@gmail.com>
2025-11-25 07:00:56 +0400

    8-ое видео

---
+++ lesson_3/main.cpp
@@ -0,0 +1,11 @@
+//* Escape последовательность это то же самое что отражение
+// \n, \t, \b
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "Привет\n\n";
+	return 0;
+}
---
+++ lesson_4/main.cpp
@@ -0,0 +1,22 @@
+//* Типы данных
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 5;
+	cout << a;
+
+	// Обьявление такое же как в java
+	// Все переменные должны быть инициализированы
+	int Age, b=2;
+
+	Age = 5;
+	b = 67;
+
+	double d = 1.34;
+	char c = 'f';
+	bool bl = true; // 1 выведет
+
+	return 0;
+}
