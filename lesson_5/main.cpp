0ca617226e055c2f303783f526557df4fcd8dd98
mehebbetfz <me.farzaliyev@gmail.com>
2025-12-22 16:36:39 +0400

    lesson_7

--- lesson_5/main.cpp
+++ lesson_5/main.cpp
@@ -7,5 +7,7 @@ using namespace std;
 int main() {
 	const int COUNT_DAYS_IN_WEEK = 7;
 
-	cout<<COUNT_DAYS_IN_WEEK<< "\n";
+	const char NEW_LINE = '\n';
+
+	cout << COUNT_DAYS_IN_WEEK << NEW_LINE;
 }
---
+++ lesson_6/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+
+// Ввод данных
+int main() {
+	int Var1, Var2;
+
+	cout << "Enter number 1: ";
+	cin >> Var1;
+
+	cout << "Enter number 2: ";
+	cin >> Var2;
+
+	cout << "Entered number 1: " << Var1 << '\n' << "Entered number 2: " << Var2 << endl;
+}
