378853b0dea2877e4b1205acb3bd1108c3e78558
mehebbetfz <me.farzaliyev@gmail.com>
2025-12-24 14:16:41 +0400

    lesson_9

--- lesson_8/main.cpp
+++ lesson_8/main.cpp
@@ -2,8 +2,20 @@
 #include <iostream>
 using namespace std;
 
+// GOTO
+int main()
+{
+	cout << "Hello 1" << endl;
 
-// Операторы
-int main() {
-	
+	goto link; // код перескочит 12, 14 строки кода
+
+	cout << "Hello 2" << endl;
+
+	cout << "Hello 3" << endl;
+
+	link: 
+
+	cout << "Hello 4" << endl;
+
+	cout << "Hello 5" << endl;
 }
---
+++ lesson_9/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+
+
+// Есть 2 типо ошибок Ошибка на этапе компиляции на этапе выполнения
+int main() {
+	
+}
